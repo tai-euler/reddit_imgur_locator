e1d883fffb4ca443a5fd13a0ef819d50dfd57d7b
tai-euler <30448277+tai-euler@users.noreply.github.com>
2017-10-01 01:08:37 +0200

    Create reddit_airdrop_locator.py

---
+++ reddit_airdrop_locator.py
@@ -0,0 +1,8 @@
+
+import praw
+reddit = praw.Reddit(client_id='your_id',
+                     client_secret="your_secret", password='your_pass',
+                     user_agent='Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+                     username='your_username')
+                     
+                     
